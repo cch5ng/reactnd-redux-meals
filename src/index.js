6978157f98aff8e3c49196a718e6983a8adaf954
Carol Chung <cch5ng@gmail.com>
2017-08-22 22:16:31 -0700

    add Provider and use to wrap App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
@@ -10,5 +11,5 @@ const store = createStore(calendar,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-ReactDOM.render(<App store={store} />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store} ><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
