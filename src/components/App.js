5a60f64814f4aa87d94b433907cf714bdfed2b5e
Carol Chung <cch5ng@gmail.com>
2017-08-22 22:17:31 -0700

    revert App contents

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,12 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
-import { addRecipe } from '../actions'
 import '../App.css';
 
 class App extends Component {
 
-  state = {
-    calendar: null
-  }
-
-  componentDidMount() {
-    const { store } = this.props
-    store.subscribe(() => {
-      this.setState(() => ({
-        calendar: store.getState()
-      }))
-    })
-  }
-
-  submitFood = () => {
-    console.log('clicked btn')
-    this.props.store.dispatch(addRecipe({
-      day: 'monday',
-      recipe: {
-        label: this.input.value
-      },
-      meal: 'breakfast'
-    }))
-
-    // clear input after start store update
-    this.input.value = ''
-  }
-
   render() {
     return (
       <div className="App">
-        <input
-            type='text'
-            ref={(input) => this.input = input}
-            placeholder="Monday's Breakfast"
-        />
-        <button onClick={this.submitFood}>Submit</button>
-
-        <pre>
-            Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
-        </pre>
+        hello world
       </div>
     );
   }
