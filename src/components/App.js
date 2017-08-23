649199ae26d8680a941425834b437736199eb9b4
Carol Chung <cch5ng@gmail.com>
2017-08-22 22:10:41 -0700

    update view, add submitFood(), subscribe to store

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
+import { addRecipe } from '../actions'
 import '../App.css';
 
 class App extends Component {
+
+  state = {
+    calendar: null
+  }
+
+  componentDidMount() {
+    const { store } = this.props
+    store.subscribe(() => {
+      this.setState(() => ({
+        calendar: store.getState()
+      }))
+    })
+  }
+
+  submitFood = () => {
+    console.log('clicked btn')
+    this.props.store.dispatch(addRecipe({
+      day: 'monday',
+      recipe: {
+        label: this.input.value
+      },
+      meal: 'breakfast'
+    }))
+
+    // clear input after start store update
+    this.input.value = ''
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input
+            type='text'
+            ref={(input) => this.input = input}
+            placeholder="Monday's Breakfast"
+        />
+        <button onClick={this.submitFood}>Submit</button>
+
+        <pre>
+            Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
+        </pre>
       </div>
     );
   }
