dd86dfa5e7d6928e5acebd62b385754560888cea
Carol Chung <cch5ng@gmail.com>
2017-08-22 21:37:11 -0700

    add redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import './index.css'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import calendar from './reducers'
+
+const store = createStore(calendar,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+console.log('store: ' + store)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
